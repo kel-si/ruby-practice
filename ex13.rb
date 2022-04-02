7412823593844e40a6a98d16f09bc927b51bf51c
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-02 13:31:15 -0600

    use ARGV

---
+++ ex13.rb
@@ -0,0 +1,12 @@
+first, second, third = ARGV # ARGV is argument variable
+
+#if the song if more than one word, put in quotation marks
+puts "Your favourite Taylor Swift album is: #{first}"
+puts "Your second favourite Taylor Swift album is: #{second}"
+puts "Your third favourite Taylor Swift album is: #{third}"
+
+
+
+
+
+
