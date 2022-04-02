78f364bee7f8cea880c41de6b26b82e6e5e7b85b
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-02 15:28:39 -0600

    if statements

---
+++ ex29.rb
@@ -0,0 +1,34 @@
+people = 29
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "So many cats!"
+end
+
+if people > cats
+  puts "Not too many cats! Phew."
+end
+
+if people < dogs
+  puts "So much drool!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs in numbers"
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs in numbers"
+end
+
+
+if people == dogs
+  puts "People are equal to dogs in numbers"
+end
