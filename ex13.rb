8817a1cdacb0b79587ff39cfc1be17961a9c4a7c
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-02 13:35:15 -0600

    use ARGV with gets.chomp

--- ex13.rb
+++ ex13.rb
@@ -5,6 +5,10 @@ puts "Your favourite Taylor Swift album is: #{first}"
 puts "Your second favourite Taylor Swift album is: #{second}"
 puts "Your third favourite Taylor Swift album is: #{third}"
 
+print "Was this a hard choice?" # yes
+response = $stdin.gets.chomp
+puts "You responded: #{response}"
+
 
 
 
