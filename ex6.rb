1edb200f8411730f72b796443a4b881e471be91a
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-02 11:52:25 -0600

    strings and text

---
+++ ex6.rb
@@ -0,0 +1,26 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+#prints the values of x and y which have been declared above
+puts x
+puts y
+
+#printing variable values inside strings
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+#declare variables
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#prints the value with false included
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = " a string with a right side."
+
+#adding two strings together; printed on one line
+puts w + e
