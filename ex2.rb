7159fe5cd054cf77e434c40aa670800a9c375a84
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-02 11:21:38 -0600

    comment exercise

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# this is called an octothorpe (what a name!), pound, or hash
+# this is how you comment in ruby
+
+puts "I could have code like this" # and the comment after is ignored.
+
+# you can also use a comment to "disable" or comment out code
+# puts "this will not run"
+
+puts "This will run."
